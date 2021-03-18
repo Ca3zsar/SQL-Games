9cee734749ee68768c190a524fd7f70b02a102e2
Cezar Todirisca <cezartodirisca@gmail.com>
2021-03-18 23:45:35 +0200

    implemented basic register model

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -6,6 +6,7 @@ namespace app\controllers;
 
 use app\core\Controller;
 use app\core\Request;
+use app\models\RegisterModel;
 
 class AuthController extends Controller
 {
@@ -17,11 +18,20 @@ class AuthController extends Controller
 
     public function register(Request $request)
     {
+        $registerModel = new RegisterModel();
         if($request->isPost())
         {
-            return 'Handle submitted data';
+
+            $registerModel->loadData($request->getBody());
+
+            if($registerModel->validate() && $registerModel->register())
+            {
+                return 'Succes';
+            }
+
+            return $this->render('register',['model' =>$registerModel]);
         }
         $this->setLayout('auth');
-        return $this->render('register');
+        return $this->render('register',['model' =>$registerModel]);
     }
 }
---
+++ models/RegisterModel.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace app\models;
+
+
+use app\core\Model;
+
+class RegisterModel extends Model
+{
+    public string $firstname;
+    public string $lastname;
+    public string $email;
+    public string $password;
+    public string $confirmPassword;
+
+    public function register()
+    {
+        echo "Creating new user";
+    }
+}
