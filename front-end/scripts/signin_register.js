fffa4136cc1192f19ddd4626972c1bbadd4616ae
Cezar Todirisca <cezartodirisca@gmail.com>
2021-03-31 21:02:19 +0300

    added new login style + fixed zoom problem

--- front-end/scripts/signin_register.js
+++ front-end/scripts/signin_register.js
@@ -9,3 +9,20 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
+
+
+
+var navbar = document.querySelector("header");
+var heightValue = window.getComputedStyle(navbar).height;
+document.getElementsByClassName("content-area")[0].style.top = heightValue;
+
+function modifyDimensions(condition)
+{
+    var navbar = document.querySelector("header")
+    var navHeight = navbar.offsetHeight;
+    document.getElementsByClassName("content-area")[0].style.top=navHeight+"px";
+}
+
+var condition = window.matchMedia("(max-width:800px)");
+modifyDimensions(condition);
+condition.addEventListener("change",modifyDimensions);
