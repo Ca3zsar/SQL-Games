81f44d13e0e047c46c0ea807bbfdb082d5e22677
Cezar Todirisca <cezartodirisca@gmail.com>
2021-04-06 21:04:46 +0300

    added compact mode

---
+++ front-end/scripts/shop_resizer.js
@@ -0,0 +1,24 @@
+var compactButton = document.getElementById("compact");
+var extendedButton = document.getElementById("extended");
+
+compactButton.addEventListener("click",function(){
+    var requirements = document.getElementsByClassName("exercise-requirement");
+    if(!requirements[0].classList.contains("compact"))
+    {
+        for(var i=0, len=requirements.length;i<len;i=i+1|0)
+        {
+            requirements[i].classList.toggle("compact",true);
+        }
+    }
+},false);
+
+extendedButton.addEventListener("click",function(){
+    var requirements = document.getElementsByClassName("exercise-requirement");
+    if(requirements[0].classList.contains("compact"))
+    {
+        for(var i=0, len=requirements.length;i<len;i=i+1|0)
+        {
+            requirements[i].classList.toggle("compact");
+        }
+    }
+},false);
