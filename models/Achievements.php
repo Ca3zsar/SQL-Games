c34cbfad8f2a09a7809a40a040ee4166408d7026
Cezar Todirisca <cezartodirisca@gmail.com>
2021-05-29 21:39:49 +0300

    Working on achievements
    Co-Authored-By: Toma Andrei-Paul <58333031+toma-andrei@users.noreply.github.com>
    Co-Authored-By: Eugeniu Buliga <61945477+EugeniuBuliga@users.noreply.github.com>

--- models/Achievements.php
+++ models/Achievements.php
@@ -15,19 +15,21 @@ class Achievements extends DBModel
     public string $required;
     public int $target;
     public int $current;
+    public int $id;
+
+    static public array $allAchievements;
 
     static public History $history;
 
     static public function getEarly()
     {
         $statement = Application::$app->db->prepare("SELECT COUNT(*) firstSolved FROM solutions WHERE idUser = :id AND firstSolver = 1");
-        $statement->bindValue(":id",Application::$app->user->id);
+        $statement->bindValue(":id", Application::$app->user->id);
 
         $statement->execute();
         $result = $statement->fetch(PDO::FETCH_ASSOC);
-        if($result != null && !empty($result)) {
-            return $result["firstSolved"];
-        }
+
+        return $result["firstSolved"];
     }
 
     static public function getSolved()
@@ -45,10 +47,24 @@ class Achievements extends DBModel
         return Application::$app->user->coins;
     }
 
+    static public function hasAchieved($achievementId)
+    {
+        $statement = Application::$app->db->prepare("SELECT * FROM userachievements WHERE idAchievement = :idAch AND idUser = :idUser");
+        $statement->bindValue(":idUser", Application::$app->user->id);
+        $statement->bindValue(":idAch", $achievementId);
+
+        $statement->execute();
+        $result = $statement->fetch();
+        if (!empty($result)) {
+            return true;
+        }
+        return false;
+    }
+
     static public function getCreated()
     {
         $statement = Application::$app->db->prepare("SELECT COUNT(*) created FROM exercises WHERE authorId = :id");
-        $statement->bindValue(":id",Application::$app->user->id);
+        $statement->bindValue(":id", Application::$app->user->id);
 
         $statement->execute();
         $result = $statement->fetch(PDO::FETCH_ASSOC);
@@ -56,7 +72,7 @@ class Achievements extends DBModel
         return $result["created"];
     }
 
-    static public function loadAchievements()
+    static public function readAchievements()
     {
         Achievements::$history = new History;
         Achievements::$history->loadHistory(Application::$app->user->id);
@@ -64,13 +80,40 @@ class Achievements extends DBModel
         $statement = Application::$app->db->prepare("SELECT * FROM achievements");
         $statement->execute();
 
-        $allAchievements = $statement->fetchAll(PDO::FETCH_CLASS,static::class);
-        foreach($allAchievements as $achievement)
-        {
-            $achievement->current = call_user_func(array(static::class,$achievement->required));
+        Achievements::$allAchievements = $statement->fetchAll(PDO::FETCH_CLASS, static::class);
+    }
+
+    static public function updateAchievements($idUser)
+    {
+        Achievements::readAchievements();
+        foreach (Achievements::$allAchievements as $achievement) {
+            $achievement->current = call_user_func(array(static::class, $achievement->required));
+            if (!Achievements::hasAchieved($achievement->id)) {
+                if ($achievement->current >= $achievement->target) {
+                    $statement = Application::$app->db->prepare("INSERT INTO userachievements(idUser, idAchievement) VALUES(:idUser, :idAch)");
+
+                    $statement->bindValue(':idUser', $idUser);
+                    $statement->bindValue(':idAch', $achievement->id);
+
+                    $statement->execute();
+                }
+            }
+        }
+    }
+
+    static public function loadAchievements()
+    {
+        Achievements::readAchievements();
+        foreach (Achievements::$allAchievements as $achievement) {
+            $achievement->current = call_user_func(array(static::class, $achievement->required));
+            if (!Achievements::hasAchieved($achievement->id)) {
+                $achievement->image = "lock.png";
+            }else{
+                $achievement->current = $achievement->target;
+            }
         }
 
-        return $allAchievements;
+        return Achievements::$allAchievements;
     }
 
     public function tableName(): string
@@ -80,7 +123,7 @@ class Achievements extends DBModel
 
     public function attributes(): array
     {
-        // TODO: Implement attributes() method.
+        return [];
     }
 
     public function primaryKey(): string
